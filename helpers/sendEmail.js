1f7e55b6c5f5a4c72e041a759eb5c69b2863f240
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-10-27 21:14:45 +0300

    Create sendEmail.js

---
+++ helpers/sendEmail.js
@@ -0,0 +1,30 @@
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+const { META_EMAIL, META_PASSWORD } = process.env;
+
+const nodemailerConfig = {
+  host: "smtp.meta.ua",
+  port: 465,
+  secure: true,
+  auth: {
+    user: META_EMAIL,
+    pass: META_PASSWORD,
+  },
+};
+
+const transporter = nodemailer.createTransport(nodemailerConfig);
+
+const sendEmail = async (data) => {
+  try {
+    const email = {
+      ...data,
+      from: META_EMAIL,
+    };
+    await transporter.sendMail(email);
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+module.exports = sendEmail;
